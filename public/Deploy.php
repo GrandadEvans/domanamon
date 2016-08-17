be373ae3842aaa973a0d00b8948d58d98960f0a9
John Evans <john@grandadevans.com>
2016-08-17 23:52:47 +0100

    Removed ASCII art from deploy script

--- public/Deploy.php
+++ public/Deploy.php
@@ -1,7 +1,1 @@
- .  ____  .   _____________________________
- |/      \|  |                             |
-[|        |] | Git Deployment Script v0.1  |
- |___==___|__/              &copy; oodavid 2012 |
-          |________________________________|
-
 <?php echo shell_exec('/usr/local/bin/bash git-pull.sh'); ?>
