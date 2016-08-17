a0ce3df8b1e92dca6862186713684f52004e4f90
John Evans <john@grandadevans.com>
2016-08-18 00:25:33 +0100

    Verbose output for Deploy script

--- public/Deploy.php
+++ public/Deploy.php
@@ -1,1 +1,40 @@
-<?php echo shell_exec('/usr/local/bin/bash git-pull.sh'); ?>
+<pre>
+<?php
+
+echo shell_exec('
+    echo -e "echo \$PWD"
+    echo    "=========="
+    echo $PWD
+    echo
+
+    echo "cd ../"
+    echo "======"
+    cd ../
+    echo
+
+    echo -e "echo -e \$PWD"
+    echo    "============="
+    echo -e $PWD"\n"
+    echo
+    
+    echo "git log -1"
+    echo "=========="
+    git log -1 
+    echo
+
+    echo "git checkout stable"
+    echo "==================="
+    git checkout stable
+    echo
+
+    echo "git pull"
+    echo "========"
+    git pull
+    echo
+
+    echo "git log -1"
+    echo "=========="
+    git log -1
+');
+?>
+</pre>
