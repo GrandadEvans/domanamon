04e42d08d63adb3befcf80e58e1a80b5a922a5c7
John Evans <john@grandadevans.com>
2016-08-27 06:57:06 +0100

    Authenticationgaa

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,11 +4,9 @@ namespace Domanamon\Http\Controllers;
 
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
-use Illuminate\Foundation\Validation\ValidatesRequests;
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-use Illuminate\Foundation\Auth\Access\AuthorizesResources;
+use Illuminate\Foundation\Validation\{ValidatesRequests, AuthorizesRequests};
 
 class Controller extends BaseController
 {
-    use AuthorizesRequests, AuthorizesResources, DispatchesJobs, ValidatesRequests;
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 }
