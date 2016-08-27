dc29178ab1da6b88bfe5b34aa1c7d3b8e87d7035
John Evans <john@grandadevans.com>
2016-08-27 13:37:20 +0100

    Queues

--- config/auth.php
+++ config/auth.php
@@ -100,7 +100,7 @@ return [
             'provider' => 'users',
             'email' => 'auth.emails.password',
             'table' => 'password_resets',
-            'expire' => 60,
+            'retry_after' => 60,
         ],
     ],
 
