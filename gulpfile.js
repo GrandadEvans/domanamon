42bd7e355cacdd7000f86290ff34f12123ac1527
John Evans <john@grandadevans.com>
2016-08-31 01:54:14 +0100

    Gulp now converts images while running with watch flag

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,6 @@
-var elixir = require('laravel-elixir');
+const elixir = require('laravel-elixir');
+const webp = require('gulp-webp');
+var Task = Elixir.Task;
 // require('laravel-elixir-copy-fonts');
 
 /*
@@ -12,6 +14,31 @@ var elixir = require('laravel-elixir');
  |
  */
 
+/*
+ | Create a new task that will convert all image file
+ */
+Elixir.extend('convertImages', function(srcDir) {
+    // An array of file type to convert to webp
+    let imagesTypes = [
+        'jpg',
+        'png',
+        'gif',
+        'jpeg'
+    ];
+    // A gulp.src path for all the image file
+    const srcFiles = srcDir +'/**/*{' + imagesTypes.join(',') + '}';
+
+    new Task('convertImages', function () {
+        // Iterate through the image files, convert them and put back in the srcDir
+        return gulp
+            .src(srcFiles)
+            .pipe(webp())
+            .pipe(gulp.dest(srcDir));
+    })
+    // If the watch flag has been passed then watch the source files
+    .watch(srcFiles);
+});
+
 elixir(function(mix) {
     mix
         .sass([
@@ -20,10 +47,7 @@ elixir(function(mix) {
         .sass([
             'app.scss'
         ], './public/css/app.css')
-        .copy(
-            './resources/images',
-            './public/images/'
-        )
+        .copy('resources/images', 'public/images/')
 
         .webpack([
             '../../../bower_components/sweetalert/dist/sweetalert.min.js'
@@ -31,7 +55,15 @@ elixir(function(mix) {
         .webpack([
             'Config.es6.js',
             'App.es6.js'
-        ], './public/js/app.js');
+        ], './public/js/app.js')
+        .convertImages('public/images');
+
 });
+
+// gulp.task('default', () =>
+//     gulp.src('src/image.jpg')
+//         .pipe(webp())
+//         .pipe(gulp.dest('dist'))
+
 
 
--- resources/views/partials/picture.blade.php
+++ resources/views/partials/picture.blade.php
@@ -1,9 +1,15 @@
 <?php
+// Normal image path
 $imagePath = '/images/' . $filename;
+// Get the extension
 $ext       = last(explode('.', $filename));
-$webpPath  = str_replace($ext, 'webp', $imagePath);
+// From the two above work out the webp image path
+$webpPath = preg_replace('/\.'.$ext.'$/', '.webp', $imagePath);
+
+// Create a blank string for the attributes
 $attrString = '';
 foreach ($attributes as $attribute => $value) {
+    // For each attribute append the attribute and value
     $attrString .= e($attribute) . '="' . e($value) . '" ';
 }
 ?>
